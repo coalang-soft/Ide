b7481de97955a1954f6e7c19c4060fbe536dbe3f
NetLamp <oplpublishergerman@gmail.com>
2017-05-13 18:31:56 +0200

    Added Installer

--- src/run.bat
+++ src/run.bat
@@ -1,8 +1,11 @@
 @echo off
 cd ..
+md lib
+copy .\src\*.* .\lib
+perl F:\Info\antlangsoft\mulpam\mulpam\mulpam.pl dir-copy F:\Info\Java\NewWorkspace\CCL\CclJars\ccl ./lib/ccl
 md bin
 cd bin
-java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl0.4.7.jar -compile ../src/main.ccl -lib F:\Info\Java\NewWorkspace\CCL\CclJars -config
+java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl0.4.7.jar -compile ../src/main.ccl -lib ..\lib -config
 copy ..\src\main.cc0 ..\bin
 java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl0.4.7.jar -lib-files "F:\Info\CCL\I_SuiteFX\bin;F:\Info\CCL\JSearchFX\bin;F:\Info\CCL\JSearch\bin;F:\Info\CCL\DragDropFX\bin;F:\Info\CCL\JVisit\bin;F:\Info\CCL\JVisitFX\bin;F:\Info\CCL\RichTextFXExtension\out\production\RichTextFXExtension;F:\Info\CCL\RichTextFXExtension\richtextfx-fat-0.7-M5.jar;F:\Info\CCL\JVisit\bin;F:\Info\CCL\FXParts\bin;" ../src/main.cc0
 ::java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl0.4.7.jar -lib-files "F:\Info\CCL\I_SuiteFX\bin;F:\Info\CCL\JSearchFX\bin;F:\Info\CCL\JSearch\bin;F:\Info\CCL\DragDropFX\bin;F:\Info\CCL\JVisit\bin;F:\Info\CCL\JVisitFX\bin;F:\Info\CCL\RichTextFXExtension\out\production\RichTextFXExtension;F:\Info\CCL\RichTextFXExtension\richtextfx-fat-0.7-M5.jar;F:\Info\CCL\JVisit\bin;F:\Info\CCL\FXParts\bin;" -archive "ide.zip"
