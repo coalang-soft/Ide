9e719c2e611d6c8a9feae887c8f9a2bb054beaab
NetLamp <oplpublishergerman@gmail.com>
2017-05-20 12:00:45 +0200

    AntLang support

--- src/run.bat
+++ src/run.bat
@@ -6,10 +6,11 @@ xcopy /y F:\Info\Java\NewWorkspace\CCL\CclJars\ccl lib\ccl
 md lib\run
 xcopy /y src\run lib\run
 md bin
+copy src\antChars.txt bin\antChars.txt
 cd bin
-java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl0.4.7.jar -compile ../src/main.ccl -lib ..\lib -config
+java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl.jar -compile ../src/main.ccl -lib ..\lib -config
 copy ..\src\main.cc0 ..\bin
-java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl0.4.7.jar -lib-files "F:\Info\CCL\I_SuiteFX\bin;F:\Info\CCL\JSearchFX\bin;F:\Info\CCL\JSearch\bin;F:\Info\CCL\DragDropFX\bin;F:\Info\CCL\JVisit\bin;F:\Info\CCL\JVisitFX\bin;F:\Info\CCL\RichTextFXExtension\out\production\RichTextFXExtension;F:\Info\CCL\RichTextFXExtension\richtextfx-fat-0.7-M5.jar;F:\Info\CCL\JVisit\bin;F:\Info\CCL\FXParts\bin;" ../src/main.cc0
-::java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl0.4.7.jar -lib-files "F:\Info\CCL\I_SuiteFX\bin;F:\Info\CCL\JSearchFX\bin;F:\Info\CCL\JSearch\bin;F:\Info\CCL\DragDropFX\bin;F:\Info\CCL\JVisit\bin;F:\Info\CCL\JVisitFX\bin;F:\Info\CCL\RichTextFXExtension\out\production\RichTextFXExtension;F:\Info\CCL\RichTextFXExtension\richtextfx-fat-0.7-M5.jar;F:\Info\CCL\JVisit\bin;F:\Info\CCL\FXParts\bin;" -archive "ide.zip"
+java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl.jar -lib-files "F:\Info\CCL\I_SuiteFX\bin;F:\Info\CCL\JSearchFX\bin;F:\Info\CCL\JSearch\bin;F:\Info\CCL\DragDropFX\bin;F:\Info\CCL\JVisit\bin;F:\Info\CCL\JVisitFX\bin;F:\Info\CCL\RichTextFXExtension\out\production\RichTextFXExtension;F:\Info\CCL\RichTextFXExtension\richtextfx-fat-0.7-M5.jar;F:\Info\CCL\JVisit\bin;F:\Info\CCL\FXParts\bin;" ../src/main.cc0
+::java -jar F:\Info\Java\NewWorkspace\CCL\CclJars\ccl.jar -lib-files "F:\Info\CCL\I_SuiteFX\bin;F:\Info\CCL\JSearchFX\bin;F:\Info\CCL\JSearch\bin;F:\Info\CCL\DragDropFX\bin;F:\Info\CCL\JVisit\bin;F:\Info\CCL\JVisitFX\bin;F:\Info\CCL\RichTextFXExtension\out\production\RichTextFXExtension;F:\Info\CCL\RichTextFXExtension\richtextfx-fat-0.7-M5.jar;F:\Info\CCL\JVisit\bin;F:\Info\CCL\FXParts\bin;" -archive "ide.zip"
 pause
 del ..\bin\*.cl2
